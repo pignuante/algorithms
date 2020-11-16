c26422f5a79a9d91a4ca633982aa305b6f32a70a
pignuante <syaoran215@naver.com>
2020-11-16 23:36:40 +0900

    2741 modify i/o

--- acmicpc/2741/main.go
+++ acmicpc/2741/main.go
@@ -8,15 +8,12 @@ import (
 
 func main() {
     rd := bufio.NewReaderSize(os.Stdin, 5)
-    wr := bufio.NewWriterSize(os.Stdout, 5)
-    defer wr.Flush()
 
     line, _, _ := rd.ReadLine()
-    value := 0
-    for _, v := range line {
-        value *= 10
-        value += int(v-'0')
-    }
+    value, _ := strconv.Atoi(string(line))
+    wr := bufio.NewWriterSize(os.Stdout, 5 * value)
+    defer wr.Flush()
+
     for i := 1; i <= value; i++ {
         wr.WriteString(strconv.Itoa(i))
         wr.WriteByte('\n')
