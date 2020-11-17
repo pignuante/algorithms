a7ea00394fc88e2362b421b981f82521ea3de3b0
pignuante <syaoran215@naver.com>
2020-11-17 14:24:15 +0900

    10871

--- acmicpc/10871/main.go
+++ acmicpc/10871/main.go
@@ -8,21 +8,22 @@ import (
 )
 
 func main() {
-    rd := bufio.NewReader(os.Stdin)
-    wt := bufio.NewWriter(os.Stdout)
-
-    defer wt.Flush()
+    rd := bufio.NewReaderSize(os.Stdin, 100000)
 
     line ,_, _ := rd.ReadLine()
     t := strings.Split(string(line), " ")
     x, _ := strconv.Atoi(t[1])
 
+    wt := bufio.NewWriterSize(os.Stdout, 100000)
+    defer wt.Flush()
+
     line2, _, _ := rd.ReadLine()
     values := strings.Split(string(line2), " ")
+
     for _, v := range values {
-        t, _ := strconv.Atoi(string(v))
+        t, _ := strconv.Atoi(v)
         if t < x {
-            wt.WriteString(string(v) + " ")
+            wt.WriteString(v + " ")
         }
     }
 
