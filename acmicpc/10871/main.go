ae3bd425de0b9642a85997b047cbb1649801475e
pignuante <syaoran215@naver.com>
2020-11-17 14:07:43 +0900

    10871

---
+++ acmicpc/10871/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "os"
+    "bufio"
+    "strings"
+    "strconv"
+)
+
+func main() {
+    rd := bufio.NewReader(os.Stdin)
+    wt := bufio.NewWriter(os.Stdout)
+
+    defer wt.Flush()
+
+    // rd.ReadLine()
+    line ,_, _ := rd.ReadLine()
+    t := strings.Split(string(line), " ")
+    x, _ := strconv.Atoi(t[1])
+
+
+
+    line2, _, _ := rd.ReadLine()
+    values := strings.Split(string(line2), " ")
+    for _, v := range values {
+        t, _ := strconv.Atoi(string(v))
+        if t < x {
+            wt.WriteString(string(v) + " ")
+        }
+    }
+
+
+
+}
+
+// func readLine(line []byte) {
+
+// arr := strings.Split(string(line), " ")
+// for _, v := range arr {
+
+// } 
+
+// }
