278cf45077a8399b053b402636c3a6913f975d42
pignuante <syaoran215@naver.com>
2020-11-24 16:02:51 +0900

    1904 save mem

--- acmicpc/1904/main.go
+++ acmicpc/1904/main.go
@@ -2,26 +2,29 @@ package main
 
 import (
     "bufio"
-    "fmt"
     "os"
     "strconv"
 )
 
-var mod = 15746
-
 func main() {
+    wt := bufio.NewWriter(os.Stdout)
+    defer wt.Flush()
     sc := bufio.NewScanner(os.Stdin)
     sc.Scan()
+
+    var mod = 15746
     N, _ := strconv.Atoi(sc.Text())
-    fmt.Println(makeDigits(N))
+    a, b := 1, 1
+    for ; N > 0; N-- {
+        a, b = b, (a+b)%mod
+    }
+    wt.WriteString(strconv.Itoa(a))
 }
 
-func makeDigits(num int) int {
-    nums := []int{0, 1, 2}
-    if num >= 3 {
-        for i := 3; i < num+1; i++ {
-            nums = append(nums, (nums[i-2]+nums[i-1])%mod)
-        }
-    }
-    return nums[num]
-}
+// func makeDigits(num int) int {
+// num1, num2 := 1, 1
+// for ; num > 0; num-- {
+// num1, num2 = num2, (num1+num2)%mod
+// }
+// return num1
+// }
