21661cb4bac9cd7d6e3042a88422e949416d723c
pignuante <syaoran215@naver.com>
2020-11-25 16:33:35 +0900

    4949 조건 정리 깔끔히 하기

--- acmicpc/4949/main.go
+++ acmicpc/4949/main.go
@@ -6,12 +6,39 @@ import (
     // "strconv"
 )
 
+type Parentheses struct {
+    bracket rune
+    before  *Parentheses
+}
+type Stack struct {
+    head *Parentheses
+    size int
+}
+
+func (stack *Stack) Push(str rune) {
+    before := stack.head
+    e := Parentheses{bracket: str, before: before}
+    stack.head = &e
+    stack.size++
+}
+
+func (stack *Stack) Pop() rune {
+    if stack.size <= 0 {
+        return '?'
+    }
+    node := stack.head
+    stack.head = node.before
+    stack.size--
+    return node.bracket
+}
+
 func main() {
     wt := bufio.NewWriter(os.Stdout)
     sc := bufio.NewScanner(os.Stdin)
     sc.Split(bufio.ScanLines)
 
     defer wt.Flush()
+
     end := "."
     for {
         sc.Scan()
@@ -19,26 +46,29 @@ func main() {
         if t == end {
             break
         }
-        parentheses := 0
-        squareBracket := 0
-        for _, v := range t{
-            if v == '(' {
-                parentheses++
-            } else if v =='[' {
-                squareBracket++
+        var stack Stack
+        flag := "yes"
+        for _, v := range t {
+            if v == '(' || v == '[' {
+                stack.Push(v)
             } else if v == ')' {
-                parentheses--
-            } else if v ==']' {
-                squareBracket--
-            }
-            if parentheses < 0 {
-                break
+                p := stack.Pop()
+                if p != '(' {
+                    flag = "no"
+                    break
+                }
+            } else if v == ']' {
+                p := stack.Pop()
+                if p != '[' {
+                    flag = "no"
+                    break
+                }
             }
         }
-        if parentheses == 0 && squareBracket == 0 {
-            wt.WriteString("yes\n")
+        if stack.size == 0 && flag=="yes"{
+            wt.WriteString("yes" + "\n")
         } else {
-            wt.WriteString("no\n")
+            wt.WriteString("no" + "\n")
         }
     }
 }
