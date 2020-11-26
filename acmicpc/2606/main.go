3feedcb247efb347dcc9b38d5f8c6661151b4c8c
pignuante <syaoran215@naver.com>
2020-11-26 15:27:19 +0900

    [2606]

---
+++ acmicpc/2606/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "strconv"
+    "strings"
+)
+
+type Computer struct {
+    networks []int
+    status   int
+}
+
+func main() {
+    wt := bufio.NewWriter(os.Stdout)
+    sc := bufio.NewScanner(os.Stdin)
+    defer wt.Flush()
+
+    sc.Scan()
+    cs, _ := strconv.Atoi(sc.Text())
+    computers := make([]Computer, cs+1, cs+1)
+
+    sc.Scan()
+    nets, _ := strconv.Atoi(sc.Text())
+    for i := 0; i < nets; i++ {
+        sc.Scan()
+        nums := strings.Fields(sc.Text())
+        a, _ := strconv.Atoi(nums[0])
+        b, _ := strconv.Atoi(nums[1])
+        computers[a].networks = append(computers[a].networks, b)
+        computers[b].networks = append(computers[b].networks, a)
+    }
+    stack := []int{}
+    DFS(computers, 1, &stack)
+    wt.WriteString(strconv.Itoa(len(stack)))
+}
+func DFS(computers []Computer, idx int, stack *[]int) {
+    computers[idx].status = 1
+    for _, network := range computers[idx].networks {
+        if computers[network].status == 0 {
+            *stack = append(*stack, network)
+            DFS(computers, network, stack)
+        }
+    }
+    computers[idx].status = 2
+}
