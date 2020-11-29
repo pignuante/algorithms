e1793044af0ca6a89f0d8d2251ea1a6f6fbf8927
pignuante <syaoran215@naver.com>
2020-11-29 17:38:01 +0900

    [18258] array

--- acmicpc/18258/main.go
+++ acmicpc/18258/main.go
@@ -7,68 +7,6 @@ import (
     "strings"
 )
 
-type Node struct {
-    num  int
-    next *Node
-}
-
-type Queue struct {
-    head, tail *Node
-    size       int
-}
-
-func (queue *Queue) Push(num int) {
-    node := Node{num: num, next: nil}
-    if queue.size == 0 {
-        queue.head = &node
-        queue.tail = &node
-    } else {
-        queue.tail.next = &node
-        queue.tail = &node
-    }
-    queue.size++
-}
-
-func (queue *Queue) Pop() (num int) {
-    if queue.size == 0 {
-        num = -1
-    } else {
-        node := queue.head
-        queue.head = node.next
-        num = node.num
-        queue.size--
-    }
-    return
-}
-
-func (queue Queue) Size() int {
-    return queue.size
-}
-
-func (queue Queue) Empty() (isEmpty int) {
-    if queue.size == 0 {
-        isEmpty = 1
-    }
-    return isEmpty
-}
-
-func (queue Queue) Front() (num int) {
-    if queue.size == 0 {
-        num = -1
-    } else {
-        num = queue.head.num
-    }
-    return
-}
-func (queue Queue) Back() (num int) {
-    if queue.size == 0 {
-        num = -1
-    } else {
-        num = queue.tail.num
-    }
-    return
-}
-
 func main() {
     wt := bufio.NewWriter(os.Stdout)
     sc := bufio.NewScanner(os.Stdin)
@@ -76,24 +14,41 @@ func main() {
     defer wt.Flush()
     sc.Scan()
     N, _ := strconv.Atoi(sc.Text())
-    var queue Queue
+    var queue []int
     for ; N > 0; N-- {
         sc.Scan()
         command := strings.Fields(sc.Text())
         switch command[0] {
         case "push":
             num, _ := strconv.Atoi(command[1])
-            queue.Push(num)
+            queue = append(queue, num)
         case "pop":
-            wt.WriteString(strconv.Itoa(queue.Pop()) + "\n")
+            num := -1
+            if len(queue) > 0 {
+                num = queue[0]
+                queue = queue[1:]
+            }
+            wt.WriteString(strconv.Itoa(num) + "\n")
         case "size":
-            wt.WriteString(strconv.Itoa(queue.Size()) + "\n")
+            wt.WriteString(strconv.Itoa(len(queue)) + "\n")
         case "empty":
-            wt.WriteString(strconv.Itoa(queue.Empty()) + "\n")
+            num := 0
+            if len(queue) == 0 {
+                num = 1
+            }
+            wt.WriteString(strconv.Itoa(num) + "\n")
         case "front":
-            wt.WriteString(strconv.Itoa(queue.Front()) + "\n")
+            num := -1
+            if len(queue) > 0 {
+                num = queue[0]
+            }
+            wt.WriteString(strconv.Itoa(num) + "\n")
         case "back":
-            wt.WriteString(strconv.Itoa(queue.Back()) + "\n")
+            num := -1
+            if len(queue) > 0 {
+                num = queue[len(queue)-1]
+            }
+            wt.WriteString(strconv.Itoa(num) + "\n")
         }
     }
 }
