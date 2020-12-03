f66899b71dad1eb3bb3885efb98640364a55e026
pignuante <syaoran215@naver.com>
2020-12-03 10:51:12 +0900

    main template

---
+++ acmicpc/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    wt := bufio.NewWriter(os.Stdout)
+    sc := bufio.NewScanner(os.Stdin)
+
+    defer wt.Flush()
+}
