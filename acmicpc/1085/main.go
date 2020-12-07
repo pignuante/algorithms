151afc5739e041284745036270204138b2d3e7fe
pignuante <syaoran215@naver.com>
2020-12-07 20:04:51 +0900

    [1085] 직사각형에서 탈출

---
+++ acmicpc/1085/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "strconv"
+)
+
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+    sc.Scan()
+    r = 0
+    for _, c := range sc.Bytes() {
+        r *= 10
+        r += int(c - '0')
+    }
+    return
+}
+func main() {
+    sc.Split(bufio.ScanWords)
+    wt := bufio.NewWriter(os.Stdout)
+    defer wt.Flush()
+
+    x, y, w, h := nextInt(), nextInt(), nextInt(), nextInt()
+    wt.WriteString(strconv.Itoa(Min(Min(w-x, x), Min(h-y, y))))
+}
+
+func Min(x, y int) (r int) {
+    if x < y {
+        r = x
+    } else {
+        r = y
+    }
+    return
+}
