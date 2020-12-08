06cad9da02bb8561257a56872413c2e4c807e2f2
pignuante <syaoran215@naver.com>
2020-12-08 15:12:10 +0900

    ,,,

--- acmicpc/main.go
+++ acmicpc/main.go
@@ -9,17 +9,10 @@ import (
 var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
 func nextInt() (r int) {
     sc.Scan()
-    r, sign := 0, 1
-
     for _, c := range sc.Bytes() {
-        if c == '-' {
-            sign = -1
-            continue
-        }
         r *= 10
         r += int(c-'0')
     }
-    r *= sign
     return
 }
 
@@ -28,5 +21,4 @@ func main() {
     wt := bufio.NewWriter(os.Stdout)
     defer wt.Flush()
 
-    wt.WriteString(strconv.Itoa(nextInt()))
 }
