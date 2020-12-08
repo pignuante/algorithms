22c5466393461902765ea16a0984915dc1c1367d
pignuante <syaoran215@naver.com>
2020-12-08 13:18:05 +0900

    nextInt negative

--- acmicpc/main.go
+++ acmicpc/main.go
@@ -3,21 +3,30 @@ package main
 import (
     "bufio"
     "os"
+    "strconv"
 )
 
 var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
 func nextInt() (r int) {
     sc.Scan()
-    r = 0
+    r, sign := 0, 1
+
     for _, c := range sc.Bytes() {
+        if c == '-' {
+            sign = -1
+            continue
+        }
         r *= 10
         r += int(c-'0')
     }
+    r *= sign
     return
 }
+
 func main() {
     sc.Split(bufio.ScanWords)
     wt := bufio.NewWriter(os.Stdout)
     defer wt.Flush()
 
+    wt.WriteString(strconv.Itoa(nextInt()))
 }
