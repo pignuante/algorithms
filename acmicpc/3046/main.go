cd777485578f92d15bea89c12bae0614836e4f7b
pignuante <syaoran215@naver.com>
2020-12-09 17:03:39 +0900

    [3046] R2

---
+++ acmicpc/3046/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "strconv"
+)
+
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+    sc.Scan()
+    for _, c := range sc.Bytes() {
+        r *= 10
+        r += int(c - '0')
+    }
+    return
+}
+
+func main() {
+    sc.Split(bufio.ScanWords)
+    wt := bufio.NewWriter(os.Stdout)
+    defer wt.Flush()
+
+    R1, S := nextInt(), nextInt()
+    wt.WriteString(strconv.Itoa(S*2 - R1))
+}
