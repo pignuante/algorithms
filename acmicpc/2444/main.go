837e269a43882e0a98f13d26d823a8d71acb0854
pignuante <syaoran215@naver.com>
2020-12-09 14:37:35 +0900

    [2444] 별찍기-7

---
+++ acmicpc/2444/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    // "strconv"
+)
+
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+    sc.Scan()
+    for _, c := range sc.Bytes() {
+        r *= 10
+        r += int(c - '0')
+    }
+    return
+}
+
+func main() {
+    sc.Split(bufio.ScanWords)
+    wt := bufio.NewWriter(os.Stdout)
+    defer wt.Flush()
+
+    N := nextInt()
+    for i := 0; i < N; i++ {
+        for s := N - i - 1; s > 0; s-- {
+            wt.WriteString(" ")
+        }
+        for x := 0; x < 2*i+1; x++ {
+            wt.WriteString("*")
+        }
+        wt.WriteByte('\n')
+    }
+    for i := N - 2; i >= 0; i-- {
+        for s := N - i - 1; s > 0; s-- {
+            wt.WriteString(" ")
+        }
+        for x := 0; x < 2*i+1; x++ {
+            wt.WriteString("*")
+        }
+        wt.WriteByte('\n')
+    }
+}
