f53a956c935e38e0d492043bf07f62c5b6c00b15
pignuante <syaoran215@naver.com>
2020-12-09 16:46:52 +0900

    [2475] 검증수

---
+++ acmicpc/2475/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "strconv"
+)
+
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+    sc.Scan()
+    for _, c := range sc.Bytes() {
+        r *= 10
+        r += int(c - '0')
+    }
+    return
+}
+
+func main() {
+    sc.Split(bufio.ScanWords)
+    wt := bufio.NewWriter(os.Stdout)
+    defer wt.Flush()
+
+    r := 0
+    for i := 0; i < 5; i++ {
+        t := nextInt()
+        r += t * t
+    }
+    wt.WriteString(strconv.Itoa(r % 10))
+}
