933f5fbc2e628e2e1b5bb7aa7b56daef837d35b8
pignuante <syaoran215@naver.com>
2020-12-09 17:45:27 +0900

    [2902] KMP는 왜  KMP일까? (조건 수정)

--- acmicpc/2902/main.go
+++ acmicpc/2902/main.go
@@ -3,21 +3,19 @@ package main
 import (
     "bufio"
     "os"
-    "strings"
 )
 
 var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
 
 func main() {
-    sc.Split(bufio.ScanWords)
     wt := bufio.NewWriter(os.Stdout)
     defer wt.Flush()
 
     sc.Scan()
-    names := strings.Split(sc.Text(), "-")
-    shorten := ""
+    names := sc.Text()
     for _, v := range names {
-        shorten += string(v[0])
+        if 'A' <= v && v <= 'Z' {
+            wt.WriteByte(byte(v))
+        }
     }
-    wt.WriteString(shorten)
 }
