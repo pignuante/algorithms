f98b5ee6193ea48c721c0984b6046f02f714238e
pignuante <syaoran215@naver.com>
2020-12-10 17:38:11 +0900

    [2914] 저작권

---
+++ acmicpc/2914/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+	sc.Scan()
+	for _, c := range sc.Bytes() {
+		r *= 10
+		r += int(c - '0')
+	}
+	return
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	wt := bufio.NewWriter(os.Stdout)
+	defer wt.Flush()
+
+	A, I := nextInt(), nextInt()
+	wt.WriteString(strconv.Itoa(A*(I-1) + 1))
+}
