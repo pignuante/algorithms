95dd643e3273ffcc986857180166447cd2fe7182
pignuante <syaoran215@naver.com>
2020-12-11 14:18:24 +0900

    [2010] 플러그

---
+++ acmicpc/2010/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "strconv"
+)
+
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+    sc.Scan()
+    for _, c := range sc.Bytes() {
+        r *= 10
+        r += int(c - '0')
+    }
+    return
+}
+
+func main() {
+    sc.Split(bufio.ScanWords)
+    wt := bufio.NewWriter(os.Stdout)
+    defer wt.Flush()
+
+    N, sum := nextInt(), 0
+    for i := 0; i < N; i++ {
+        sum += nextInt()
+    }
+    wt.WriteString(strconv.Itoa(sum - N + 1))
+}
