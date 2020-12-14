24c51977e063e18f9252d2a4c883b784bd84fa33
pignuante <syaoran215@naver.com>
2020-12-14 21:37:09 +0900

    [11726] 2xN타일링

---
+++ acmicpc/11726/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "strconv"
+)
+
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+    sc.Scan()
+    for _, c := range sc.Bytes() {
+        r *= 10
+        r += int(c - '0')
+    }
+    return
+}
+
+func main() {
+    sc.Split(bufio.ScanWords)
+    wt := bufio.NewWriter(os.Stdout)
+    defer wt.Flush()
+
+    n := nextInt()
+    a, b := 1, 1
+    for i := 1; i < n; i++ {
+        a ,b = b, (a+b)%10007
+    }
+    wt.WriteString(strconv.Itoa(b))
+}
+
