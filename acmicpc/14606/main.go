7034b35e7caf862104de4ee4be8c02575eeb57a7
pignuante <syaoran215@naver.com>
2020-12-17 22:05:56 +0900

    [14606] 피자(Small)

---
+++ acmicpc/14606/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "strconv"
+)
+
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+    sc.Scan()
+    for _, c := range sc.Bytes() {
+        r *= 10
+        r += int(c - '0')
+    }
+    return
+}
+
+func main() {
+    sc.Split(bufio.ScanWords)
+    wt := bufio.NewWriter(os.Stdout)
+    defer wt.Flush()
+
+    n := nextInt()
+    wt.WriteString(strconv.Itoa(GetHappy(n)))
+}
+
+func GetHappy(n int) int {
+    return n * (n + 1) / 2
+}
