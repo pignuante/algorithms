1c84800b8d95b25bd70500089184c3762dcecaad
pignuante <syaoran215@naver.com>
2020-12-29 21:06:06 +0900

    [16430] 제리와 톰

---
+++ acmicpc/16430/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var wt *bufio.Writer = bufio.NewWriter(os.Stdout)
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+	sc.Scan()
+	for _, c := range sc.Bytes() {
+		r *= 10
+		r += int(c - '0')
+	}
+	return
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wt.Flush()
+
+	A, B := nextInt(), nextInt()
+	wt.WriteString(strconv.Itoa(B - A))
+	wt.WriteByte(' ')
+	wt.WriteString(strconv.Itoa(B))
+}
