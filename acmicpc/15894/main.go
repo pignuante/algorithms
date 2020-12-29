0a9a8c52bf4c78899fe70b859c5a5331c46243a4
pignuante <syaoran215@naver.com>
2020-12-29 20:31:43 +0900

    [15894] 수학은 체육과목 입니다

---
+++ acmicpc/15894/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var wt *bufio.Writer = bufio.NewWriter(os.Stdout)
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+	sc.Scan()
+	for _, c := range sc.Bytes() {
+		r *= 10
+		r += int(c - '0')
+	}
+	return
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wt.Flush()
+
+	n := nextInt()
+	wt.WriteString(strconv.Itoa(n * 4))
+}
