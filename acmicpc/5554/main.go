02c724d95942178dadddd26fd4d717c063cca91e
pignuante <syaoran215@naver.com>
2020-12-29 10:35:16 +0900

    [5554] 심부름 가는 길

---
+++ acmicpc/5554/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+	sc.Scan()
+	for _, c := range sc.Bytes() {
+		r *= 10
+		r += int(c - '0')
+	}
+	return
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	wt := bufio.NewWriter(os.Stdout)
+	defer wt.Flush()
+
+	time := 0
+	for i := 0; i < 4; i++ {
+		time += nextInt()
+	}
+	wt.WriteString(strconv.Itoa(time / 60))
+	wt.WriteByte('\n')
+	wt.WriteString(strconv.Itoa(time % 60))
+}
