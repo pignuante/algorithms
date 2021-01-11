6b38ac0ac407b6f843084c83b87f47d37695138e
pignuante <syaoran215@naver.com>
2021-01-11 21:57:57 +0900

    [9372] 상근이의 여행

---
+++ acmicpc/9372/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "strconv"
+)
+
+var wt *bufio.Writer = bufio.NewWriter(os.Stdout)
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func nextInt() (r int) {
+    sc.Scan()
+    for _, c := range sc.Bytes() {
+        r *= 10
+        r += int(c - '0')
+    }
+    return
+}
+
+func main() {
+    sc.Split(bufio.ScanWords)
+    defer wt.Flush()
+
+    T := nextInt()
+    for i := 0; i < T; i++ {
+        N, M := nextInt(), nextInt()
+        for j := 0; j < M; j++ {
+            _, _ = nextInt(), nextInt()
+        }
+        wt.WriteString(strconv.Itoa(N - 1))
+        wt.WriteByte('\n')
+    }
+}
